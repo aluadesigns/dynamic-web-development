4e0bc0d3c71aa4371c4ef2ec59575e7983b710b0
aluamangeldikyzy <70909755+aluamangeldikyzy@users.noreply.github.com>
2026-01-27 17:31:24 -0500

    first push

---
+++ class-demos/week-1/script.js
@@ -0,0 +1,11 @@
+windeow.onload = () => {
+    console.log("page is fully loaded");
+    document
+        .getElementById("")
+        .innerHTML("updated content with javascript");
+    init();
+}
+
+function init() {
+
+}
